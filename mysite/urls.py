e2721d32bd4bbb8973d6e4054cbf6d48111e52b0
xylenium <dittoni@yahoo.fr>
2023-06-05 18:20:04 +0200

    initial commit

--- mysite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from . import views
-
-urlpatterns=[
-    path('',views.index,name='index'),
-    path('profile/',views.profile,name='profile'),
-    path('',include('social_django.urls')),
-    path('logout/',views.logout,name='logout'),
-]
